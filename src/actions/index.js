306173e6daa7405e5e2598f2d60488779bdfe1f1
Steve Butler <stevebutler2210@gmail.com>
2017-04-14 18:34:28 +0100

    End of day commit - end of Lecture 56

---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+const API_KEY = 'bf11a16936d92e0d6e7a6dfe96ebe0f2';
+
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -7,15 +7,25 @@ export default class SearchBar extends Component {
     this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    //this.onFormSubmit = this.onFormSubmit.bind(this);
   }
   
   onInputChange(event) {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // fetch weather data
+    
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form
+        onSubmit={this.onFormSubmit}
+        className="input-group">
         <input
           placeholder="Enter city to search for a five day forecast"
           className="form-control"

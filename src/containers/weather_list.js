758814a016f9e71e8a5ff8a5df3997c9cf5bc413
Steve Butler <stevebutler2210@gmail.com>
2017-04-15 18:33:40 +0100

    broke chart out into separate component, added charts for each section, and added units

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    //const pressures = cityData.list.map(weather => weather.main.pressure);
-    //const humidities = cityData.list.map(weather => weather.main.humidity);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name} >
         <td>{name}</td>
-        <td>
-          <Sparklines height={60} width={100} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
-        </td>
+        <td><Chart data={temps} color="red" units="K" /></td>
+        <td><Chart data={pressures} color="blue" units="hPa" /></td>
+        <td><Chart data={humidities} color="green" units="%" /></td>
       </tr>
     );
   }
@@ -27,9 +25,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>

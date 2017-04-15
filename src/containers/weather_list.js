2ad9f6781f16f741cde27e7b2e2c0b801d8b97e3
Steve Butler <stevebutler2210@gmail.com>
2017-04-15 18:38:00 +0100

    secondary map on temp to convert from Kelvin to Celsius

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
@@ -5,14 +6,14 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273.15);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name} >
         <td>{name}</td>
-        <td><Chart data={temps} color="red" units="K" /></td>
+        <td><Chart data={temps} color="red" units="C" /></td>
         <td><Chart data={pressures} color="blue" units="hPa" /></td>
         <td><Chart data={humidities} color="green" units="%" /></td>
       </tr>
@@ -25,7 +26,7 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature (K)</th>
+            <th>Temperature (C)</th>
             <th>Pressure (hPa)</th>
             <th>Humidity (%)</th>
           </tr>

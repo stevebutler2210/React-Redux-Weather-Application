24a1b3b40f27e7eebdd756d177c77290313a1d11
Steve Butler <stevebutler2210@gmail.com>
2017-04-15 17:54:20 +0100

    Installed sparkline and set up basic chart implementation

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    //const pressures = cityData.list.map(weather => weather.main.pressure);
+    //const humidities = cityData.list.map(weather => weather.main.humidity);
+
+    return (
+      <tr key={name} >
+        <td>{name}</td>
+        <td>
+          <Sparklines height={60} width={100} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+      </tr>
+    );
+  }
+  
   render() {
     return (
       <table className="table table-hover">
@@ -13,7 +32,9 @@ class WeatherList extends Component {
             <th>Humidity</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
       </table>
     );
   }
